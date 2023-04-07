41a062baaf194b1521be9ba962700a1e039f910e
vburdyk <burdikv1991@gmail.com>
2023-04-07 13:03:14 +0300

    new repo

---
+++ Python-Titans-group-master/lectures/flask-app/models/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(255), nullable=True)
+    last_name = db.Column(db.String(255), nullable=True)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
+
+
+class Article(db.Model):
+    __tablename__ = "articles"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow())
